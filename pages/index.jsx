6217a92f9477c294bf317b07fa3d42ffd2504421
Srinuvas17 <srinumokka94@gmail.com>
2025-06-20 14:37:13 +0530

    Fixed module path issues for Vercel build

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { Button } from "@/components/ui/button";
-import { Card, CardContent } from "@/components/ui/card";
+import { Button } from "../components/ui/button";
+import { Card, CardContent } from "../components/ui/card";
 import Image from "next/image";
 
 export default function HomePage() {
